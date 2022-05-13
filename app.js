e77f1a5dfff52f1bd2c73e15d290113f76dd10ae
theluposki <theluposki@gmail.com>
2022-05-13 00:59:43 -0300

    add options

--- app.js
+++ app.js
@@ -5,21 +5,37 @@ createApp({
     return {
       text: "",
       qrCode: "CodeGenerate_qr",
+      viewText: true,
+      viewWifi: false,
+      ssid: null,
+      security: "WPA",
+      password: null,
     };
   },
   mounted() {
     this.generate();
   },
   methods: {
+    setViewWifi() {
+      this.viewText = false;
+      this.viewWifi = true;
+    },
+    setViewText() {
+      this.viewWifi =  false;
+      this.viewText = true;
+    },
     generate() {
-      const data = "Code";
-      const baseUrl =
-        "https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=";
+      const baseUrl = "https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=";
+      const wifiString = `WIFI:S:${this.ssid};T:${this.security};P:${this.password};`
+
+      if(this.viewWifi) {
+        return this.qrCode = `${baseUrl}${wifiString}`;
+      }
       this.qrCode = `${baseUrl}${this.text}`;
     },
     async pasteClip() {
       this.text = await navigator.clipboard.readText();
-      this.generate()
+      this.generate();
     },
   },
 }).mount("#app");
